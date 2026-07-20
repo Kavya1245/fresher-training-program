70692f52faa3f5fa5433dad97cadb4434e75c259
Kavya1245 <kavya22s145@gmail.com>
2026-07-20 09:45:47 +0530

    Day-1: GitHub Copilot Chat verified working - generated double() function

--- Day-1/basics.js
+++ Day-1/basics.js
@@ -3,3 +3,8 @@ function getEvenNumbers(nums) {
     return nums.filter(n => n % 2 === 0);
 }
 console.log(getEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+// function to double every number in an array
+function double(nums) {
+    return nums.map(n => n * 2);
+}
